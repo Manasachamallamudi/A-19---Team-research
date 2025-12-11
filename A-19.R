06dca23b641949219dccb8e50a6206cf507acdd5
Alekhyarayani <ar24agr@herts.ac.uk>
2025-12-11 12:45:51 +0000

    Update A-19.R

--- A-19.R
+++ A-19.R
@@ -5,3 +5,10 @@ library(ggplot2)
 
 
 loans_raw <- read_excel("Student Loan Debt by School 2020-2021.xlsx")
+school_debt_us <- loans_raw %>%
+  group_by(School, `School Type`) %>%
+  summarise(
+    total_amount     = sum(`$ of Loans Originated`, na.rm = TRUE),
+    total_recipients = sum(Recipients, na.rm = TRUE),
+    avg_loan         = total_amount / total_recipients,
+    .groups = "drop"
